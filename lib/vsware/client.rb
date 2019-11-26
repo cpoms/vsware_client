fbe3770cece6c22bf025ef07cf443ec45f263a6e
Andrei Diaconu <andrei.diaconu@cpoms.co.uk>
2019-11-26 15:31:01 +0000

    Endpoint for student images

--- lib/vsware/client.rb
+++ lib/vsware/client.rb
@@ -18,37 +18,66 @@ module Vsware
     studentclasses 
     teachers
     teachinggroup
-    students_images
-    teachers_images
     ).each do |resource|
-      define_method resource do |options ={}|
-        resource = resource.tr("_","/")
-        #resource = resource.delete("_")
-        response = http_get_request(resource, params: options)
+      define_method resource do |options = {}|
+        resource = resource.tr("_", "/")
+        response = http_get_request(resource, params: options, path_append: "/fetch")
 
-        case response.status
-        when 200
-          puts resource
-          response = JSON.parse(response.body)
-          raise "#{response["errorMessage"]}" if response["errorMessage"]
-          
-          response["payload"]
-        when 404
-          raise "not found"
-        end
+        handle_response(response)
       end
+    end
+
+    def student_images(options = {})
+      response = http_get_request(
+        "students/images",
+        params: options
+      )
+      handle_response(response)
+    end
+
+    def teacher_images(options = {})
+      response = http_get_request(
+        "teachers/images",
+        params: options
+      )
+      handle_response(response)
+    end
+
+    def student_image(options = {})
+      response = http_get_request(
+        "studentimages",
+        params: options,
+        path_append: "/fetch/#{options.delete(:student_id)}"
+      )
+
+      handle_response(response)
     end
 
     private
     def http_get_request path, options = {}
+      options[:path_append] ||= ""
+
       auth_params = {
         apikey: @api_key,
         password: @password,
         username: @user,
         vendor: @vendor
       }
-        Excon.get("#{@url}#{path =~ /\// ? path : "#{path}/fetch"}?#{URI.encode_www_form(auth_params.merge(options[:params]))}")
+
+      Excon.get("#{@url}#{path}#{options[:path_append]}?#{URI.encode_www_form(auth_params.merge(options[:params]))}")
     end
 
+    def handle_response(response)
+      case response.status
+      when 200
+        # puts resource
+        response = JSON.parse(response.body)
+        raise "#{response["errorMessage"]}" if response["errorMessage"]
+
+        response["payload"]
+      when 404
+        raise "not found"
+      end
+    end
   end
 end
